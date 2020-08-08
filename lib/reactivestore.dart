d12e0129759fd2d1996cd0dc35d8d117ea447df0
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-08 14:08:51 +0200

    Created the ReactiveStore Class

--- lib/reactivestore.dart
+++ lib/reactivestore.dart
@@ -1,7 +1,53 @@
 library reactivestore;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reactivestore/model/store_model.dart';
+
+class ReactiveStore extends StatefulWidget{
+
+  final Widget child;
+  ReactiveStore({@required this.child});
+
+  @override
+  ReactiveStoreState createState() {
+    return new ReactiveStoreState();
+  }
+
+}
+class ReactiveStoreState extends State<ReactiveStore>{
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<StoreModel>(
+      create: (context) => StoreModel(),
+      child:  new MaterialApp(
+        home: widget.child,
+      )
+    );
+  }
+}
+
+
+class UpdateUI extends StatefulWidget{
+  
+  final Function builder;
+  UpdateUI({@required this.builder});
+
+  @override
+  UpdateUIState createState(){
+    return new UpdateUIState();
+  }
+
+}
+
+class UpdateUIState extends State<UpdateUI>{
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<StoreModel>(
+      builder: widget.builder
+    );
+  }
+  
 }
