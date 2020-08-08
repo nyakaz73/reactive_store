2bdc4f833d67bbe0f17e715bb18eb388e9cb7060
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-08 15:53:27 +0200

    UpdateUI Widget Test

--- test/reactivestore_test.dart
+++ test/reactivestore_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:reactivestore/model/store_model.dart';
 import 'package:reactivestore/reactivestore.dart';
 
+
 void main() {
   testWidgets('ReactiveStore Widget with a child', (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
@@ -22,5 +23,45 @@ void main() {
     expect(titleFinder, findsOneWidget);
    
   });
-  
+
+
+  testWidgets('UpdateUI Widget with a callback builder function', (WidgetTester tester) async {
+    
+
+    await tester.pumpWidget(ReactiveStore(
+      child: new Scaffold(
+        body: new Column(
+          children: <Widget>[
+            new Builder(
+              builder: (BuildContext context){
+                //final model = Provider.of<StoreModel>(context,listen: false);//This does not work since it will be currently building
+                final model = StoreModel();
+                model.add({'name':'Daniel','surname':'Moe','age':7});
+                model.add({'name':'Marcus','surname':'Doe','age':72});
+                model.add({'name':'Jessica','surname':'Smith','age':12});
+                return Text('Hell');
+              }
+            ),
+            new UpdateUI(
+              builder: (context, dialogModel, child) =>
+                  dialogModel.itemsList != null
+                      ? Text(
+                          dialogModel.totalItems.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 25.0,
+                              color: Colors.green),
+                        )
+                      : null),
+          ],
+        ),
+      )
+    ));
+
+    final fullNameFinder = find.text('3');
+    expect(fullNameFinder, findsNothing);
+    //expect(fullNameFinder, findsOneWidget);
+
+  });
 }
+  
