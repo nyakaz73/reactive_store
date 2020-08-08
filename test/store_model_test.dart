86984a487fd65c3fc049debcb1a17330a623a769
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-08 15:57:41 +0200

    Testing Remove items Model

--- test/store_model_test.dart
+++ test/store_model_test.dart
@@ -14,4 +14,21 @@ void main() {
     expect(model.totalItems, 3);
   });
 
+  test('testing remove items', () {
+    final model = StoreModel();
+    final totalItems = model.totalItems;
+    expect(totalItems, 0);
+    model.add({'name':'Daniel','surname':'Moe','age':7});
+    model.add({'name':'Marcus','surname':'Doe','age':72});
+    model.add({'name':'Jessica','surname':'Smith','age':12});
+    expect(model.totalItems, 3);
+    model.removeAll();
+    expect(model.totalItems, 0);
+    model.add({'name':'Marcus','surname':'Doe','age':72});
+    model.add({'name':'Jessica','surname':'Smith','age':12});
+    model.remove(1);
+    expect(model.totalItems, 1);
+  });
+
+
 }
