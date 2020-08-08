4cd953351a96e8d4b23646c761fd97de08548ba6
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-08 14:03:28 +0200

    Added getter in model

--- lib/model/store_model.dart
+++ lib/model/store_model.dart
@@ -1,31 +1,21 @@
-
-import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 
 class StoreModel extends ChangeNotifier{ // This is an Observable we can subscribe to its changes.
-  Map<dynamic,dynamic> dataMap =new HashMap();
   List<Map> _mainList = new List();
+
+  int get totalItems => this._mainList.length;
+  List<Map> get itemsList => this._mainList;
 
   /*Implementing a Composition Design Pattern on a List ADT
     Adding additional operations to the already existing list
   */
   void add(Map data){
-    print('Adding data'+data.toString());
     this._mainList.add(data);
-    print('Data added in model');
-    this._mainList.forEach((element) {
-      print(element);
-      print('NOw the name');
-      print(element['name']);
-    });
-    print(this._mainList);
-    // This call tells the widgets that are listening to this model to rebuild.
     notifyListeners();
   }
 
   void remove(int index){
     this._mainList.removeAt(index);
-    // This call tells the widgets that are listening to this model to rebuild.
     notifyListeners();
   }
 
@@ -33,8 +23,4 @@ class StoreModel extends ChangeNotifier{ // This is an Observable we can subscri
     this._mainList.clear();
     notifyListeners();
   }
-
-  int get totalItems => this._mainList.length;
-  List<Map> get itemsList => this._mainList;
-
 }
