fa76ee8f722badd1862c10eaabbbd889c1a98949
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-16 20:03:15 +0200

    Formated the store model

--- lib/model/store_model.dart
+++ lib/model/store_model.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
-class StoreModel extends ChangeNotifier{ // This is an Observable we can subscribe to its changes.
-  
+
+class StoreModel extends ChangeNotifier {
+  // This is an Observable we can subscribe to its changes.
+
   List<Map> _mainList = new List();
 
   int get totalItems => this._mainList.length;
   List<Map> get itemsList => this._mainList;
-  
+
   /*Implementing a Composition Design Pattern on a List ADT
     Adding additional operations to the already existing list
   */
-  
-   of(BuildContext context, bool listen){
-    return Provider.of<StoreModel>(context,listen:listen); //accessing store 
+
+  of(BuildContext context, bool listen) {
+    return Provider.of<StoreModel>(context, listen: listen); //accessing store
   }
-  void add(Map data){
+
+  void add(Map data) {
     this._mainList.add(data);
     notifyListeners();
   }
 
-  void remove(int index){
+  void remove(int index) {
     this._mainList.removeAt(index);
     notifyListeners();
   }
 
-  void removeAll(){
+  void removeAll() {
     this._mainList.clear();
     notifyListeners();
   }
