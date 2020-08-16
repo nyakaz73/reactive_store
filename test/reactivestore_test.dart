1874bc88eef2959c647b41b54320c6a33894e396
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-16 20:04:35 +0200

    Formattted testss

--- test/reactivestore_test.dart
+++ test/reactivestore_test.dart
@@ -3,48 +3,38 @@ import 'package:flutter/material.dart';
 import 'package:reactivestore/model/store_model.dart';
 import 'package:reactivestore/reactivestore.dart';
 
-
 void main() {
   testWidgets('ReactiveStore Widget with a child', (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
     await tester.pumpWidget(ReactiveStore(
-      child: new Scaffold(
-        body: Container(
-          child:Text('ReactiveStore with a child Text Widget')
-        ),
-      )
-    ));
+        child: new Scaffold(
+      body: Container(child: Text('ReactiveStore with a child Text Widget')),
+    )));
 
     // Create the Finders.
     final titleFinder = find.text('ReactiveStore with a child Text Widget');
-    
+
     // Use the `findsOneWidget` matcher provided by flutter_test to
     // verify that the Text widgets appear exactly once in the widget tree.
     expect(titleFinder, findsOneWidget);
-   
   });
 
-
-  testWidgets('UpdateUI Widget with a callback builder function', (WidgetTester tester) async {
-    
-
+  testWidgets('UpdateUI Widget with a callback builder function',
+      (WidgetTester tester) async {
     await tester.pumpWidget(ReactiveStore(
       child: new MaterialApp(
-        home: new Scaffold(
-          body: new Column(
-            children: <Widget>[
-              new Builder(
-                builder: (BuildContext context){
-                  //final model = Provider.of<StoreModel>(context,listen: false);//This does not work since it will be currently building
-                  final model = StoreModel();
-                  model.add({'name':'Daniel','surname':'Moe','age':7});
-                  model.add({'name':'Marcus','surname':'Doe','age':72});
-                  model.add({'name':'Jessica','surname':'Smith','age':12});
-                  return Text(model.itemsList.length.toString());
-
-                }
-              ),
-              new UpdateUI(
+          home: new Scaffold(
+        body: new Column(
+          children: <Widget>[
+            new Builder(builder: (BuildContext context) {
+              //final model = Provider.of<StoreModel>(context,listen: false);//This does not work since it will be currently building
+              final model = StoreModel();
+              model.add({'name': 'Daniel', 'surname': 'Moe', 'age': 7});
+              model.add({'name': 'Marcus', 'surname': 'Doe', 'age': 72});
+              model.add({'name': 'Jessica', 'surname': 'Smith', 'age': 12});
+              return Text(model.itemsList.length.toString());
+            }),
+            new UpdateUI(
                 builder: (context, dialogModel, child) =>
                     dialogModel.itemsList != null
                         ? Text(
@@ -55,16 +45,13 @@ void main() {
                                 color: Colors.green),
                           )
                         : null),
-            ],
-          ),
-        )
-      ),
+          ],
+        ),
+      )),
     ));
 
     final fullNameFinder = find.text('3');
     //expect(fullNameFinder, findsNothing);
     expect(fullNameFinder, findsOneWidget);
-
   });
 }
-  
--- test/store_model_test.dart
+++ test/store_model_test.dart
@@ -8,9 +8,9 @@ void main() {
     final totalItems = model.totalItems;
 
     expect(totalItems, 0);
-    model.add({'name':'Daniel','surname':'Moe','age':7});
-    model.add({'name':'Marcus','surname':'Doe','age':72});
-    model.add({'name':'Jessica','surname':'Smith','age':12});
+    model.add({'name': 'Daniel', 'surname': 'Moe', 'age': 7});
+    model.add({'name': 'Marcus', 'surname': 'Doe', 'age': 72});
+    model.add({'name': 'Jessica', 'surname': 'Smith', 'age': 12});
     expect(model.totalItems, 3);
   });
 
@@ -18,28 +18,26 @@ void main() {
     final model = StoreModel();
     final totalItems = model.totalItems;
     expect(totalItems, 0);
-    model.add({'name':'Daniel','surname':'Moe','age':7});
-    model.add({'name':'Marcus','surname':'Doe','age':72});
-    model.add({'name':'Jessica','surname':'Smith','age':12});
+    model.add({'name': 'Daniel', 'surname': 'Moe', 'age': 7});
+    model.add({'name': 'Marcus', 'surname': 'Doe', 'age': 72});
+    model.add({'name': 'Jessica', 'surname': 'Smith', 'age': 12});
     expect(model.totalItems, 3);
     model.removeAll();
     expect(model.totalItems, 0);
-    model.add({'name':'Marcus','surname':'Doe','age':72});
-    model.add({'name':'Jessica','surname':'Smith','age':12});
+    model.add({'name': 'Marcus', 'surname': 'Doe', 'age': 72});
+    model.add({'name': 'Jessica', 'surname': 'Smith', 'age': 12});
     model.remove(1);
     expect(model.totalItems, 1);
   });
 
-  test('testing getting items in list', (){
+  test('testing getting items in list', () {
     final model = StoreModel();
     final data = model.itemsList;
-    model.add({'name':'Marcus','surname':'Doe','age':72});
-    model.add({'name':'Jessica','surname':'Smith','age':12});
+    model.add({'name': 'Marcus', 'surname': 'Doe', 'age': 72});
+    model.add({'name': 'Jessica', 'surname': 'Smith', 'age': 12});
     model.remove(1);
     data.forEach((element) {
-      expect(element, {'name':'Marcus','surname':'Doe','age':72});
+      expect(element, {'name': 'Marcus', 'surname': 'Doe', 'age': 72});
     });
-
   });
-
 }
