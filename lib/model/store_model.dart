21819439e6ffa6076c4f6647129cb52882ee7b5f
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-16 18:16:44 +0200

    added the of method to provide the instance  and listen of the Provider

--- lib/model/store_model.dart
+++ lib/model/store_model.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/cupertino.dart';
-
+import 'package:provider/provider.dart';
 class StoreModel extends ChangeNotifier{ // This is an Observable we can subscribe to its changes.
+  
   List<Map> _mainList = new List();
 
   int get totalItems => this._mainList.length;
   List<Map> get itemsList => this._mainList;
-
+  
   /*Implementing a Composition Design Pattern on a List ADT
     Adding additional operations to the already existing list
   */
+  
+   of(BuildContext context, bool listen){
+    return Provider.of<StoreModel>(context,listen:listen); //accessing store 
+  }
   void add(Map data){
     this._mainList.add(data);
     notifyListeners();
